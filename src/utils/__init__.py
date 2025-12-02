38bbe9a1d4bfeaed263939e7cc4b116b5629303a
Solomon Njogo <85402865+solomon-njogo@users.noreply.github.com>
2025-12-02 14:21:09 +0300

    added logging feature

---
+++ src/utils/__init__.py
@@ -0,0 +1,8 @@
+"""
+Utility modules for the Swahili Text Processing Pipeline.
+"""
+
+from .logger import get_logger, setup_logging, TRACE_LEVEL
+
+__all__ = ['get_logger', 'setup_logging', 'TRACE_LEVEL']
+
